d890e5a89b99b577abb9e9ec1b02f414672237c0
streafy <streafyhs@gmail.com>
2025-08-15 22:32:29 +0700

    Применил AndroidApplicationConventionPlugin в app модуле

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,34 +1,17 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
-}
-
-kotlin {
-    jvmToolchain(11)
+    alias(libs.plugins.shiftrandomusers.android.application)
 }
 
 android {
     namespace = "com.streafy.shiftrandomusers"
-    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.streafy.shiftrandomusers"
-        minSdk = 26
-        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
-    }
-
-    buildFeatures {
-        compose = true
     }
 }
 
 dependencies {
     implementation(libs.activity.compose)
-    implementation(platform(libs.compose.bom))
-    implementation(libs.material3)
-    implementation(libs.ui.tooling.preview)
-    debugImplementation(libs.ui.tooling)
 }
