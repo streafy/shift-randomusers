585435ab4b5225c5e5ba10a6a4a2f3336dcd8b99
streafy <streafyhs@gmail.com>
2025-08-16 23:33:20 +0700

    Добавил компонент RetryButtonBox

---
+++ core/ui/src/main/kotlin/com/streafy/shiftrandomusers/core/ui/components/RetryButtonBox.kt
@@ -0,0 +1,21 @@
+package com.streafy.shiftrandomusers.core.ui.components
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun RetryButtonBox(onRetry: () -> Unit) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Button(onClick = onRetry) {
+            Text("Retry")
+        }
+    }
+}
