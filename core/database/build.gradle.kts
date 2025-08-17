c099c2bab46b00390454aae99142fffbda4bcc29
streafy <streafyhs@gmail.com>
2025-08-17 19:56:48 +0700

    Добавил зависимости для room

--- core/database/build.gradle.kts
+++ core/database/build.gradle.kts
@@ -1,7 +1,14 @@
 plugins {
     alias(libs.plugins.shiftrandomusers.android.library)
+    alias(libs.plugins.ksp)
 }
 
 android {
     namespace = "com.streafy.shiftrandomusers.core.database"
+}
+
+dependencies {
+    implementation(libs.room.runtime)
+    ksp(libs.room.compiler)
+    implementation(libs.room.ktx)
 }
