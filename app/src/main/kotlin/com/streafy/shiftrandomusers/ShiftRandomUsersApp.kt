2a26aeb1ccd0cc53b7104d9f52d541f815bf68dc
streafy <streafyhs@gmail.com>
2025-08-17 17:56:26 +0700

    Добавил необходимые аннотации для hilt

---
+++ app/src/main/kotlin/com/streafy/shiftrandomusers/ShiftRandomUsersApp.kt
@@ -0,0 +1,7 @@
+package com.streafy.shiftrandomusers
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ShiftRandomUsersApp : Application()
--- feature/user-list/src/main/kotlin/com/streafy/shiftrandomusers/feature/userlist/domain/usecases/UpdateUsersUseCase.kt
+++ feature/user-list/src/main/kotlin/com/streafy/shiftrandomusers/feature/userlist/domain/usecases/UpdateUsersUseCase.kt
@@ -1,7 +1,8 @@
 package com.streafy.shiftrandomusers.feature.userlist.domain.usecases
 
 import com.streafy.shiftrandomusers.feature.userlist.domain.UserListRepository
+import javax.inject.Inject
 
-class UpdateUsersUseCase(
+class UpdateUsersUseCase @Inject constructor(
     repository: UserListRepository
 ) : suspend () -> Unit by repository::updateUsers
