6b64e5b456c6cbab13b240c683e41b6ae5f28e6b
streafy <streafyhs@gmail.com>
2025-08-17 18:48:14 +0700

    Поправил неправильный endpoint

--- core/network/src/main/kotlin/com/streafy/shiftrandomusers/core/network/RandomUserApi.kt
+++ core/network/src/main/kotlin/com/streafy/shiftrandomusers/core/network/RandomUserApi.kt
@@ -10,7 +10,7 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface RandomUserApi {
-    @GET("/")
+    @GET("api")
     suspend fun getUsers(
         @Query("results") results: Int
     ): RemoteResponse
